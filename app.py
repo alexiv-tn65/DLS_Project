f2d6c85be2055378420cb0086f8c07cff7529074
alex <you@example.com>
2023-07-05 12:11:23 +0300

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
+
 
 
 
@@ -14,7 +16,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 # TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-
+TOKEN = '5806405592:AAF2kMEn7hBQb0iEjZ1Mom4xnVkulXrYOJo'
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
@@ -40,7 +42,7 @@ async def style_transfer_begin(message: types.Message):
 
 @dp.message_handler(state='*', commands='abort')
 @dp.message_handler(Text(equals='abort', ignore_case=True), state='*')
-async def cancel_handler(message: types.Message, state: FSMContext):
+async def abort_handler(message: types.Message, state: FSMContext):
     current_state = await state.get_state()
     if current_state is None:
         return
@@ -58,6 +60,18 @@ async def process_photo_main(message: types.Message, state: FSMContext):
     await FotoState.next()
     await bot.send_message(message.chat.id, "Now send style photo.")
 
+
+@dp.message_handler(state=FotoState.photo_main)
+async def error_photo_main(message: types.Message):
+    await message.reply("Error. I need a photo.")
+
+@dp.message_handler(state=FotoState.photo_style, content_types=['photo'])
+async def process_photo_style(message: types.Message, state: FSMContext):
+    pass
+
+@dp.message_handler(state=FotoState.photo_style)
+async def error_photo_style(message: types.Message):
+    await message.reply("Error. I need a style photo.")
 
 @dp.message_handler(content_types=['text'])
 async def get_text_messages(message: types.Message):
