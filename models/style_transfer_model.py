123847864e863d2af42f71ae3c920743fa2ddc7f
alex <you@example.com>
2023-07-07 16:45:12 +0300

    intermediate

--- models/style_transfer_model.py
+++ models/style_transfer_model.py
@@ -15,50 +15,66 @@ from models.loss_functions import StyleLoss, ContentLoss,
 from models.image_preparation import Normalization
 
 
-class VGGStyleTransfer(nn.Module):
-    def __init__(self, device: Optional[str] = None):
+class VGG(nn.Module):
+    def __init__(self, device):
         super().__init__()
 
         if device is None:
-            self.device: str = 'cuda' if torch.cuda.is_available() else 'cpu'
+            # self.device: str = 'cuda' if torch.cuda.is_available() else 'cpu'
+            self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            torch.set_default_device(self.device)
         else:
             self.device: str = device
 
-        self.image_size: int = 512 if self.device == 'cuda' else 128
-        self.image_transforms = transforms.Compose([
-            transforms.Resize((self.image_size, self.image_size)),
-            transforms.ToTensor()
-        ])
-        self.vgg = models.vgg19(weights='VGG19_Weights.DEFAULT').features.to(self.device).eval()
+        # self.image_size: int = 512 if self.device == 'cuda' else 128
 
-    def build_model(self, content_image, style_image):
+        # desired size of the output image
+        self.imsize = 512 if torch.cuda.is_available() else 128  # use small size if no GPU
+
+        # self.image_transforms = transforms.Compose([
+        #     transforms.Resize((self.image_size, self.image_size)),
+        #     transforms.ToTensor()
+        # ])
+
+        self.loader = transforms.Compose([
+        transforms.Resize(self.imsize),  # scale imported image
+        transforms.ToTensor()])  # transform it into a torch tensor
+
+
+        # self.vgg = models.vgg19(weights='VGG19_Weights.DEFAULT').features.to(self.device).eval()
+        self.cnn = models.vgg19(pretrained=True).features.eval()
+
+
+    def get_style_model_and_losses(self, cnn, normalization_mean, normalization_std,
+                                   style_img, content_img,
+                                   content_layers=content_layers_default,
+                                   style_layers=style_layers_default):
+
         # desired depth layers to compute style/content losses :
         content_layers = ['conv_4']
         style_layers = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
 
+        # normalization module
+        normalization = Normalization(normalization_mean, normalization_std)
+
+        # just in order to have an iterable access to or list of content/style
+        # losses
         content_losses = []
         style_losses = []
 
-        # use recommended mean and std
-        cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(self.device)
-        cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(self.device)
-
-        # normalization module
-        normalization = Normalization(cnn_normalization_mean, cnn_normalization_std).to(self.device)
-
-        # assuming that cnn is a nn.Sequential, so we make a new nn.Sequential
+        # assuming that ``cnn`` is a ``nn.Sequential``, so we make a new ``nn.Sequential``
         # to put in modules that are supposed to be activated sequentially
         model = nn.Sequential(normalization)
 
         i = 0  # increment every time we see a conv
-        for layer in self.vgg.children():
+        for layer in cnn.children():
             if isinstance(layer, nn.Conv2d):
                 i += 1
                 name = 'conv_{}'.format(i)
             elif isinstance(layer, nn.ReLU):
                 name = 'relu_{}'.format(i)
-                # The in-place version doesn't play very nicely with the ContentLoss
-                # and StyleLoss we insert below. So we replace with out-of-place
+                # The in-place version doesn't play very nicely with the ``ContentLoss``
+                # and ``StyleLoss`` we insert below. So we replace with out-of-place
                 # ones here.
                 layer = nn.ReLU(inplace=False)
             elif isinstance(layer, nn.MaxPool2d):
@@ -72,14 +88,14 @@ class VGGStyleTransfer(nn.Module):
 
             if name in content_layers:
                 # add content loss:
-                target = model(content_image).detach()
+                target = model(content_img).detach()
                 content_loss = ContentLoss(target)
                 model.add_module("content_loss_{}".format(i), content_loss)
                 content_losses.append(content_loss)
 
             if name in style_layers:
                 # add style loss:
-                target_feature = model(style_image).detach()
+                target_feature = model(style_img).detach()
                 style_loss = StyleLoss(target_feature)
                 model.add_module("style_loss_{}".format(i), style_loss)
                 style_losses.append(style_loss)
@@ -93,30 +109,35 @@ class VGGStyleTransfer(nn.Module):
 
         return model, style_losses, content_losses
 
-    def run_style_transfer(self, content_image,
-                           style_image,
-                           image_path: str = None,
-                           num_steps=100,
-                           style_weight=1000000,
-                           content_weight=1):
-        model, style_losses, content_losses = self.build_model(content_image, style_image)
+    def run_style_transfer(self, cnn, normalization_mean, normalization_std,
+                           content_img, style_img, input_img, num_steps=300,
+                           style_weight=1000000, content_weight=1):
+        """Run the style transfer."""
+        print('Building the style transfer model..')
+        model, style_losses, content_losses = get_style_model_and_losses(cnn,
+            normalization_mean, normalization_std, style_img, content_img)
 
-        # We want to optimize the input and not the model parameters, so we
+        # We want to optimize the input and not the model parameters so we
         # update all the requires_grad fields accordingly
-        content_image.requires_grad_(True)
+        input_img.requires_grad_(True)
+        # We also put the model in evaluation mode, so that specific layers
+        # such as dropout or batch normalization layers behave correctly.
+        model.eval()
         model.requires_grad_(False)
 
-        optimizer = optimizer = optim.LBFGS([content_image])
+        optimizer = get_input_optimizer(input_img)
 
-        for _ in tqdm(range(num_steps)):
+        print('Optimizing..')
+        run = [0]
+        while run[0] <= num_steps:
 
             def closure():
                 # correct the values of updated input image
                 with torch.no_grad():
-                    content_image.clamp_(0, 1)
+                    input_img.clamp_(0, 1)
 
                 optimizer.zero_grad()
-                model(content_image)
+                model(input_img)
                 style_score = 0
                 content_score = 0
 
@@ -131,20 +152,26 @@ class VGGStyleTransfer(nn.Module):
                 loss = style_score + content_score
                 loss.backward()
 
+                run[0] += 1
+                if run[0] % 50 == 0:
+                    print("run {}:".format(run))
+                    print('Style Loss : {:4f} Content Loss: {:4f}'.format(
+                        style_score.item(), content_score.item()))
+                    print()
+
                 return style_score + content_score
 
             optimizer.step(closure)
 
         # a last correction...
         with torch.no_grad():
-            content_image.clamp_(0, 1)
+            input_img.clamp_(0, 1)
 
-        if image_path:
-            save_image(content_image[0], image_path)
-        return content_image
+        return input_img
 
-    def load_image(self, image_path):
-        image = Image.open(image_path)
+    def image_loader(self, image_path):
+        image = Image.open(image_name)
         # fake batch dimension required to fit network's input dimensions
-        image = self.image_transforms(image).unsqueeze(0)
-        return image.to(self.device, torch.float)
+        image = loader(image).unsqueeze(0)
+        return image.to(device, torch.float)
+
