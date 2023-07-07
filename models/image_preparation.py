7e998621a7d2e556072aa59ff3e6de0f9ee9533c
alex <you@example.com>
2023-07-07 13:32:21 +0300

    intermediate commit

--- models/image_preparation.py
+++ models/image_preparation.py
@@ -1,39 +1,16 @@
+import torch.nn as nn
 
-
-
-
-
-def img_loader(img, device):
-    """
-    This function loads and pre-porcesses images for
-    style transfer needs.
-    """
-    PIL_img = Image.open(img)
-    max_dim = max(PIL_img.size)
-    max_allow_dim = 256
-    if max_dim > max_allow_dim:
-        scale_ratio = max_allow_dim / max_dim
-        loader = transforms.Compose([transforms.Resize(
-            (int(PIL_img.size[1] * scale_ratio),
-             int(PIL_img.size[0] * scale_ratio))),
-            transforms.ToTensor()])
-    else:
-        loader = transforms.ToTensor()
-    tensor_img = loader(PIL_img).unsqueeze(0).to(device)
-    return tensor_img
-
-
-
+# create a module to normalize input image so we can easily put it in a
+# ``nn.Sequential``
 class Normalization(nn.Module):
-    """
-    This class represent normalization layer for VGG head
-    which has been trained on IMAGENET.
-    """
-
-    def __init__(self):
+    def __init__(self, mean, std):
         super(Normalization, self).__init__()
-        self.mean = torch.tensor([0.485, 0.456, 0.406]).view(-1, 1, 1)
-        self.std = torch.tensor([0.229, 0.224, 0.225]).view(-1, 1, 1)
+        # .view the mean and std to make them [C x 1 x 1] so that they can
+        # directly work with image Tensor of shape [B x C x H x W].
+        # B is batch size. C is number of channels. H is height and W is width.
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
 
     def forward(self, img):
+        # normalize ``img``
         return (img - self.mean) / self.std
--- models/loss functions.py
+++ models/loss functions.py
@@ -1,32 +1,28 @@
 import torch.nn as nn
 
+from models.common import gram_matrix
 
 
 class ContentLoss(nn.Module):
-    """
-    Content loss layer
-    """
 
-    def __init__(self, target):
+    def __init__(self, target,):
         super(ContentLoss, self).__init__()
+        # we 'detach' the target content from the tree used
+        # to dynamically compute the gradient: this is a stated value,
+        # not a variable. Otherwise the forward method of the criterion
+        # will throw an error.
         self.target = target.detach()
-        self.loss = F.mse_loss(self.target, self.target)
 
     def forward(self, input):
-        if input.shape == self.target.shape:
-            self.loss = F.mse_loss(input, self.target)
+        self.loss = F.mse_loss(input, self.target)
         return input
 
 
 class StyleLoss(nn.Module):
-    """
-    Style Loss layer
-    """
 
     def __init__(self, target_feature):
         super(StyleLoss, self).__init__()
         self.target = gram_matrix(target_feature).detach()
-        self.loss = F.mse_loss(self.target, self.target)
 
     def forward(self, input):
         G = gram_matrix(input)
